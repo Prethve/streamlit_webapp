a188113110f58c3f07478ade9a9d2295c086e4ca
Prethve Deev <diamondspree431@gmail.com>
2025-10-12 14:27:39 +0800

    Conditional Content for login and Unique welcome text for Users

--- mylibrary.py
+++ mylibrary.py
@@ -1,6 +1,33 @@
+import streamlit as st
+
 def calculate_total( oranges, apples):
     return 2*oranges + 3*apples
 
 def plastic_bags_price(bags):
     """one bag 0.1, two bags 0.4, three bags 0.9 """
     return 0.1*bags*bags
+
+#### Styling functions ####
+# Title 
+def title_text(title, caption):
+    top_space = '<div style="padding: 50px 10px;"></div>'
+    bottom_space = '<div style="padding: 100px 10px;"></div>'
+    return {
+        st.markdown(top_space, unsafe_allow_html=True),
+        st.title(title, anchor=None, help=None, width='content'),
+        st.write(caption),
+        st.markdown(bottom_space, unsafe_allow_html=True)
+    }
+    
+    
+
+# Main Page intro columns
+def column(feature, text):
+    container = st.container(key={feature}, horizontal_alignment='center', vertical_alignment='center', width='stretch')
+    return {
+        st.header(feature, width='content'),
+        st.caption(text, width='content')
+    }
+
+
+    
--- pages/marketplace.py
+++ pages/marketplace.py
@@ -1,21 +1,40 @@
 import streamlit as st
 import mylibrary
 
-st.header('Marketplace')
+if not st.user.is_logged_in:
+    with st.container(horizontal_alignment='center', key='title', width='stretch'):
+        mylibrary.title_text('Welcome To Streamlit', 'kafsdfj askjdfa;klsdjf ajsfaskdjfaksd fjas;iejasgl;kjakdsgjklahsdguhdfjkghskdfhga sdfghaouks hasdv')
 
-st.title('Welcome To App', anchor=None, help=None, width="stretch")
-st.text_input("Enter the quantity of African Oranges $2 each", key="oranges")
-st.number_input("Enter the quantity of Fuji Apples $3 each", min_value=0, max_value=10, step=1, key="apples")
-st.slider("How many plastic bags do you want", min_value=0, max_value=3, step=1, key="bags")
-bags_cost = mylibrary.plastic_bags_price(st.session_state.bags)
-st.write("Your plastic bags will cost $" + str(bags_cost))
+    col1, col2, col3 = st.columns(3, width='stretch', border=True)
+    with st.container(horizontal_alignment='center', key='features', width='stretch'):
+        with col1:
+            mylibrary.column('Unbeatable Deals', 'asdgfhj fasdfa gdfhsdfghdfhsdf shf sdfsdfg hfh')
+        
+        with col2:
+            mylibrary.column('Diverse Collection', 'asdgfhj fasdfa gdfhsdfghdfhsdf shf sdfsdfg hfh')
+        
+        with col3:
+            mylibrary.column('Convenient Shopping', 'asdgfhj fasdfa gdfhsdfghdfhsdf shf sdfsdfg hfh')
 
-if st.button('Get Total'):
-    st.write('Calculating Your Total ...')
-    oranges = int(st.session_state.oranges)
-    apples = int(st.session_state.apples)
-    result = mylibrary.calculate_total(oranges, apples)
-    total = bags_cost + result
-    st.write('Please pay {:.2f}'.format(total))
 else:
-    st.write('Please Key In The Quantity')
+    with st.container(horizontal_alignment='center', key='main-container', width='stretch'):
+        st.title(f'Welcome, {st.user.name}!', anchor=None, help=None)
+        st.text_input("Enter the quantity of African Oranges $2 each", key="oranges")
+        st.number_input("Enter the quantity of Fuji Apples $3 each", min_value=0, max_value=10, step=1, key="apples")
+        st.slider("How many plastic bags do you want", min_value=0, max_value=3, step=1, key="bags")
+        bags_cost = mylibrary.plastic_bags_price(st.session_state.bags)
+        st.write("Your plastic bags will cost $" + str(bags_cost))
+
+        if st.button('Get Total'):
+            st.write('Calculating Your Total ...')
+            oranges = int(st.session_state.oranges)
+            apples = int(st.session_state.apples)
+            result = mylibrary.calculate_total(oranges, apples)
+            total = bags_cost + result
+            st.write('Please pay {:.2f}'.format(total))
+        else:
+            st.write('Please Key In The Quantity')
+
+
+
+
