b4dffc56828749b7719c2c487aab09c39f2a39f8
Prethve Deev <diamondspree431@gmail.com>
2025-10-16 17:33:30 +0800

    Data from CSV to Dict, Shoe tiles

--- mylibrary.py
+++ mylibrary.py
@@ -1,4 +1,11 @@
 import streamlit as st
+# import pandas as pd
+import datetime as date
+import csv
+
+CSV_PATH = '/Users/deevprethve/Documents/School/SUTD/10.025 Computational Thinking for Design/streamlit_app/shOE_DTB.csv'
+
+### Key Functions ####
 
 def calculate_total( oranges, apples):
     return 2*oranges + 3*apples
@@ -7,8 +14,45 @@ def plastic_bags_price(bags):
     """one bag 0.1, two bags 0.4, three bags 0.9 """
     return 0.1*bags*bags
 
+def discounts():
+    today = date.today()
+    print(today)
+
+class Shoe:
+    def __init__(self, shoe_name, brand, size_available, price, color, stock, seller_username):
+        self.shoe_name = shoe_name
+        self.brand = brand
+        self.size_available = size_available
+        self.price = price
+        self.color = color
+        self.stock = stock
+        self.seller_username = seller_username
+
+
+def extract_data():
+    # shoe_db = pd.read_csv(CSV_PATH)
+    shoe_db = {}
+    with open(CSV_PATH, mode ='r')as file:
+        csv_file = csv.reader(file)
+        for lines in csv_file:
+            shoe_dict = {
+                'shoe_name':lines[0],
+                'shoe_id':lines[1],
+                'brand':lines[2],
+                'size_available':lines[3],
+                'price':lines[4],
+                'color':lines[5],
+                'stock':lines[6],
+                'seller_username':lines[7],
+            }
+            shoe_db[lines[0]] = shoe_dict
+    return shoe_db #{st.write(shoe_db)}
+
+            
+
 #### Styling functions ####
 # Title 
+
 def title_text(title, caption):
     top_space = '<div style="padding: 50px 10px;"></div>'
     bottom_space = '<div style="padding: 100px 10px;"></div>'
@@ -19,8 +63,6 @@ def title_text(title, caption):
         st.markdown(bottom_space, unsafe_allow_html=True)
     }
     
-    
-
 # Main Page intro columns
 def column(feature, text):
     container = st.container(key={feature}, horizontal_alignment='center', vertical_alignment='center', width='stretch')
@@ -30,4 +72,5 @@ def column(feature, text):
     }
 
 
+print(extract_data())
     
--- pages/marketplace.py
+++ pages/marketplace.py
@@ -3,7 +3,7 @@ import mylibrary
 
 if not st.user.is_logged_in:
     with st.container(horizontal_alignment='center', key='title', width='stretch'):
-        mylibrary.title_text('Welcome To Streamlit', 'kafsdfj askjdfa;klsdjf ajsfaskdjfaksd fjas;iejasgl;kjakdsgjklahsdguhdfjkghskdfhga sdfghaouks hasdv')
+        mylibrary.title_text('Welcome To Sneako', 'kafsdfj askjdfa;klsdjf ajsfaskdjfaksd fjas;iejasgl;kjakdsgjklahsdguhdfjkghskdfhga sdfghaouks hasdv')
 
     col1, col2, col3 = st.columns(3, width='stretch', border=True)
     with st.container(horizontal_alignment='center', key='features', width='stretch'):
@@ -19,21 +19,37 @@ if not st.user.is_logged_in:
 else:
     with st.container(horizontal_alignment='center', key='main-container', width='stretch'):
         st.title(f'Welcome, {st.user.name}!', anchor=None, help=None)
-        st.text_input("Enter the quantity of African Oranges $2 each", key="oranges")
-        st.number_input("Enter the quantity of Fuji Apples $3 each", min_value=0, max_value=10, step=1, key="apples")
-        st.slider("How many plastic bags do you want", min_value=0, max_value=3, step=1, key="bags")
-        bags_cost = mylibrary.plastic_bags_price(st.session_state.bags)
-        st.write("Your plastic bags will cost $" + str(bags_cost))
+        shoe_db = mylibrary.extract_data()
 
-        if st.button('Get Total'):
-            st.write('Calculating Your Total ...')
-            oranges = int(st.session_state.oranges)
-            apples = int(st.session_state.apples)
-            result = mylibrary.calculate_total(oranges, apples)
-            total = bags_cost + result
-            st.write('Please pay {:.2f}'.format(total))
-        else:
-            st.write('Please Key In The Quantity')
+
+        num_columns = 3
+        columns = st.columns(num_columns, width='stretch')
+
+        for i, (key, value) in enumerate(shoe_db.items()):
+            with columns[i % num_columns]: # Distribute tiles across columns
+                tile_cont = st.container(height='stretch' ,border=True)
+                tile_cont.text(key)
+                tile_cont.subheader(f"{value['price']}") # Use the dictionary key as a tile title
+                tile_cont.caption(f"{value['brand']}")
+            
+        
+        
+        
+        # st.text_input("Enter the quantity of African Oranges $2 each", key="oranges")
+        # st.number_input("Enter the quantity of Fuji Apples $3 each", min_value=0, max_value=10, step=1, key="apples")
+        # st.slider("How many plastic bags do you want", min_value=0, max_value=3, step=1, key="bags")
+        # bags_cost = mylibrary.plastic_bags_price(st.session_state.bags)
+        # st.write("Your plastic bags will cost $" + str(bags_cost))
+
+        # if st.button('Get Total'):
+        #     st.write('Calculating Your Total ...')
+        #     oranges = int(st.session_state.oranges)
+        #     apples = int(st.session_state.apples)
+        #     result = mylibrary.calculate_total(oranges, apples)
+        #     total = bags_cost + result
+        #     st.write('Please pay {:.2f}'.format(total))
+        # else:
+        #     st.write('Please Key In The Quantity')
 
 
 
